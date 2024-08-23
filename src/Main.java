c73ad82985a7b1c11894fe393fab4328121f711e
homework <Sakurovadaria@gmail.com>
2024-08-23 23:23:21 +0500

    All tasks done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import javax.naming.PartialResultException;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1 and 2");
-        int[] numbers = new int[12];
+        int[] numbers = new int[3];
         numbers[0] = 1;
         numbers[1] = 2;
         numbers[2] = 3;
@@ -20,15 +23,54 @@ public class Main {
             }
         }
         System.out.println();
-
-
-        System.out.println("Task 1");
-        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        weights[0] = 90;
-        int januaryWeight = weights[0];
-        int january = 0;
+        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 95, 98, 96, 100};
         for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
+            System.out.print(weights[i]);
+            if (weights[i] != 100) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Task 3");
+        int[] numbers3 = new int[3];
+        numbers3[0] = 1;
+        numbers3[1] = 2;
+        numbers3[2] = 3;
+        for (int i = numbers3.length - 1; i >= 0; i--) {
+            System.out.print(numbers3[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        float[] numbers4 = {1.57f, 7.654f, 9.986f};
+        for (int i = numbers4.length - 1; i >= 0; i--) {
+            System.out.print(numbers4[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        int[] weights2 = {90, 91, 93, 92, 85, 87, 88, 89, 95, 98, 96, 100};
+        for (int i = weights2.length - 1; i >= 0; i--) {
+            System.out.print(weights2[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Task 4");
+        int[] taskForeNumbers = {1, 2, 3};
+        for (int i = 0; i < 3; i++) {
+            if (taskForeNumbers[i] % 2 != 0) {
+                taskForeNumbers[i] += 1;
+            }
+            System.out.print(taskForeNumbers[i]);
+            if (taskForeNumbers[i] != 4) {
+                System.out.print(", ");
+            }
         }
     }
 }
